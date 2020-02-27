c7ae4f337bd0612c3218c2730854286da079447e
eymiandrade <56372502+eymiandrade@users.noreply.github.com>
2020-02-27 14:59:27 -0600

    Examen
    Unidad 2

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,1 +1,17 @@
-<html
+<DOCTYPE html>
+<html>
+<head>
+<title> Ejercicio numeros </title>
+</head>
+<body>
+<h2> Desarrollar los siguientes datos </h2>
+<p>
+Construir un algoritmo en el cual se ingresen 3 números y se sepa cuál es el mayor de los 3"
+</p>
+<?php>
+
+</?>
+
+</body>
+
+</html>
